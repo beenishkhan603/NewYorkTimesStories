c1425faefc6a684049bf041c77c1b2c026c53de1
Beenish Khan <beenishkhan603@gmail.com>
2023-10-24 14:00:00 +0500

    Add: loader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+// external imports
 import React, { useEffect, useState } from 'react';
+import { ThreeCircles } from 'react-loader-spinner';
 import axios from 'axios';
-
+// internal imports
 import StoryCard from './components/storyCard';
+// css
+import './App.css';
 
 function App() {
 	const [stories, setStories] = useState([]);
@@ -9,19 +13,33 @@ function App() {
 
 	useEffect(() => {
 		axios
-			.get(`${process.env.REACT_APP_BACKEND_URL}/top-stories`)
+			.get(`${process.env.REACT_APP_BACKEND_URL}/top-stories`) // api calling to get the stories
 			.then((response) => {
 				setStories(response.data.results);
+				setIsLoading(false);
 			})
 			.catch((error) => {
 				console.error('Error fetching data:', error);
+				setIsLoading(false);
 			});
 	}, []);
 
 	return (
-		<div style={{ justifyContent: 'center', textAlign: 'center' }}>
+		<div className="App">
 			<h1>New York Times Top Stories</h1>
-			<StoryCard stories={stories} />
+			<br />
+			{isLoading ? (
+				<ThreeCircles
+					height="70"
+					width="70"
+					color="#0039a6"
+					wrapperStyle={{ justifyContent: 'center' }}
+					visible={true}
+					ariaLabel="three-circles-rotating"
+				/>
+			) : (
+				<StoryCard stories={stories} />
+			)}
 		</div>
 	);
 }
